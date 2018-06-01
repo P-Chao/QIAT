4e2db318dded40f6a64fda7fa97df6aa0513a849
PC <1377318286@qq.com>
2018-06-01 14:28:46 +0800

    use matplot to show 000001 index

--- qiat.py
+++ qiat.py
@@ -1,3 +1,9 @@
 import tushare as ts
+import matplotlib.pyplot as plt
 
-print (ts.get_hist_data('600848'))
+df = ts.get_hist_data('sh', start = '2016-01-01')
+df.to_excel('stock_sh.xlsx')
+df.close.plot()
+ax = plt.gca()
+ax.invert_xaxis()
+plt.show()
